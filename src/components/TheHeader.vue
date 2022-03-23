0b0d2ebd96a2c63c01773dc03bbeeed1401507f0
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-03-23 10:28:53 -0600

    added header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex gap-4 bg-gray-200 py-4">
+    <TheLogo />
+    <div class="flex gap-2 items-center ml-2">
+      <span class="text-2xl font-semibold text-black">SEPHORA</span>
+    </div>
+    <div>
+  </header>
+</template>
+
+
+ <script setup>
+import TheLogo from "./TheLogo.vue";
+import TheNavBar from "./TheNavBar.vue";
+</script>
