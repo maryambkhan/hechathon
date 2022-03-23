639ce1142029ae1bc3c6a0645eeb14c9462e4784
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-03-23 17:00:33 -0600

    fixed navbar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -4,7 +4,7 @@
     <ul class="">
       <a href="#" class="px-10 font-semibold" >New</a>
       <a href="#" class="px-10 font-semibold">Brands</a>
-      <a href="#" class="px-140font-semibold">Makeup</a>
+      <a href="#" class="px-10 font-semibold">Makeup</a>
       <a href="#" class="px-10 font-semibold">SkinCare</a>
        <a href="#" class="px-10 font-semibold">Gifts</a>
       <a href="#" class="p-2 font-bold text-black bg-red-200 rounded-sm hover:bg-red-500">Sign Up</a>
