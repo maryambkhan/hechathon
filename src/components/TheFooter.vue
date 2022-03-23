c1aef97c6af6cece2c27ac46153cb35eaf1b362c
Mahalakshmi <mahait07@gmail.com>
2022-03-23 11:18:48 -0600

    footer styled

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,7 @@
 <template>
-<footer class="grid place-items-center text-center mx-1">
-  <p>&copy; Maryam Khan 2022</p>
+<div class="mt-4">
+<footer class="grid place-items-center text-center  bg-black">
+  <p class="text-white text-xl py-8">&copy; Maryam Khan 2022</p>
 </footer>
+</div>
 </template>
