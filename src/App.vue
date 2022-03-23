767c4456be1385e0e78f44af02e9b4d497c296b6
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-03-23 10:57:35 -0600

    added styling to herosection

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <TheHeader />
   <TheLogo />
   
-  <!-- Page Header -->
-  <header class="flex flex-col items-center justify-center h-40 py-4">
-    <h1 class="text-5xl uppercase md:text-6xl lg:text-7xl">
-    </h1>
-  </header>
    <HeroSection />
     <TheFooter />
 </template>
