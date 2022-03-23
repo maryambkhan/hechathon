6ca352d61a112ab6d7ab8b3e189527eb66f00c6f
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-03-23 10:42:14 -0600

    added hero section image

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
   <header class="flex flex-col items-center justify-center h-40 py-4">
     <h1 class="text-5xl uppercase md:text-6xl lg:text-7xl">
     </h1>
-    <TheFooter />
+   
   </header>
+   <HeroSection />
+    <TheFooter />
 </template>
 
 
 <script setup>
 import TheHeader from "./components/TheHeader.vue";
+import HeroSection from "./components/HeroSection.vue";
 import TheFooter from "./components/TheFooter.vue";
 </script>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    <img src="../assets/images/hero.jpeg" alt="sephora hero image" />
+</div>
+</template>
